bfccf1fb2517a987f2637f402e6edd0bc364636b
AlphaSpartanA <tj.effinger@gmail.com>
2014-05-29 08:25:49 -0700

    Added default window procedure functionality and comments to custom wundow procedure.

--- Windows Programming Sandbox/WindowsProgrammingSandbox/MainWindow.cpp
+++ Windows Programming Sandbox/WindowsProgrammingSandbox/MainWindow.cpp
@@ -4,17 +4,18 @@ LRESULT CALLBACK windowProcedure(HWND windowHandle, UINT message, WPARAM wParam,
 {
 	switch (message)
 	{
-	case WM_LBUTTONDOWN:
+	case WM_LBUTTONDOWN: // handle left mouse button click
 		MessageBox(0, L"WM_LBUTTON DOWN message", L"Msg", MB_OK);
 		return 0;
-	case WM_KEYDOWN:
+	case WM_KEYDOWN: // handle the escape key
 		if (wParam == VK_ESCAPE)
 			DestroyWindow(windowHandle);
 		return 0;
-	case WM_DESTROY:
+	case WM_DESTROY: // handle exiting the application
 		PostQuitMessage(0);
 		return 0;
 	}
+	return DefWindowProc(windowHandle, message, wParam, lParam); // invoke the default window procedure for standard window functionality
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR cmdLine, int showCmd) // enter the program loop
