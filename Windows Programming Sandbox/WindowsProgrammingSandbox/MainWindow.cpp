dad7d74444d19888e9e4490fb7ce22763a107bc7
AlphaSpartanA <tj.effinger@gmail.com>
2014-06-10 08:48:43 -0700

    Implemented functionality to draw lines.

--- Windows Programming Sandbox/WindowsProgrammingSandbox/MainWindow.cpp
+++ Windows Programming Sandbox/WindowsProgrammingSandbox/MainWindow.cpp
@@ -1,8 +1,24 @@
 #include <Windows.h>
+#include <vector>
+
+using namespace std;
 
 //stores handles to the main window and application instance globally
 HWND mainWindow = 0;
 HINSTANCE appInstance = 0;
+HDC hdc;
+PAINTSTRUCT ps;
+
+struct Line
+{
+	POINT p0;
+	POINT P1;
+};
+
+vector<Line> gLines;
+Line gLine;
+
+bool gMouseDown = false;
 
 //Define the main window procedure
 LRESULT CALLBACK windowProcedure(HWND windowHandle, UINT message, WPARAM wParam, LPARAM lParam) 
@@ -10,7 +26,50 @@ LRESULT CALLBACK windowProcedure(HWND windowHandle, UINT message, WPARAM wParam,
 	switch (message)
 	{
 	case WM_LBUTTONDOWN: // handle left mouse button click
-		MessageBox(0, L"WM_LBUTTON DOWN message", L"Msg", MB_OK);
+		SetCapture(windowHandle);
+		gMouseDown = true;
+
+		gLine.p0.x = LOWORD(lParam);
+		gLine.p0.y = HIWORD(lParam);
+
+		return 0;
+
+	case WM_MOUSEMOVE:
+		if (gMouseDown)
+		{
+			gLine.P1.x = LOWORD(lParam);
+			gLine.P1.y = HIWORD(lParam);
+			InvalidateRect(windowHandle, 0, true);
+		}
+		return 0;
+
+	case WM_LBUTTONUP:
+		ReleaseCapture();
+		gMouseDown = false;
+
+		gLine.P1.x = LOWORD(lParam);
+		gLine.P1.y = HIWORD(lParam);
+
+		gLines.push_back(gLine);
+		InvalidateRect(windowHandle, 0, true);
+
+		return 0;
+
+	case WM_PAINT:
+		hdc = BeginPaint(windowHandle, &ps);
+
+		if (gMouseDown)
+		{
+			MoveToEx(hdc, gLine.p0.x, gLine.p0.y, 0);
+			LineTo(hdc, gLine.P1.x, gLine.P1.y);
+		}
+
+		for (int i = 0; i < gLines.size(); ++i)
+		{
+			MoveToEx(hdc, gLines[i].p0.x, gLines[i].p0.y, 0);
+			LineTo(hdc, gLines[i].P1.x, gLines[i].P1.y);
+		}
+		EndPaint(windowHandle, &ps);
 		return 0;
 	case WM_KEYDOWN: // handle the escape key
 		if (wParam == VK_ESCAPE)
@@ -67,6 +126,5 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR cmdLine, i
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
-
-
+	
 }
